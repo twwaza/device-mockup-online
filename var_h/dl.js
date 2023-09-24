69ee6f7b3a9a51b7d2c2feaf19085cde5d71c899
twwaza <139862962+twwaza@users.noreply.github.com>
2023-09-24 08:42:45 +0700

    Create dl.js

---
+++ var_h/dl.js
@@ -0,0 +1,10 @@
+
+	<script type="text/javascript">
+  function var_dl(id) {
+    document.getElementById(id).style.display='block';
+    }
+    function resetBtn() {
+        history.go(0);
+  }
+  
+</script>
